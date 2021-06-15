b0cb8d94856d8e2c698f753463e238926673ed99
Cristian Deleon <libaration@gmail.com>
2021-06-14 21:57:54 -0400

    commit tree 07119690e21bd98f89bfc307bc84ddd773af24e9

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [],
+    };
+  }
+  async componentDidMount() {
+    const response = await fetch('http://api.open-notify.org/astros.json');
+    const data = await response.json();
+    this.setState({ people: data.people });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.people.map((person) => {
+          return person.name;
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
